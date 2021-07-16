d21ff5970bb538752b72d62093818d59e07213e6
BrunoSSantana <brunoosouza15@gmail.com>
2021-07-16 17:03:36 -0300

    Configure connection on database

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@ import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 
-import './database';
 import './shared/container';
-import { router } from './routes';
-import { AppError } from './shared/errors/AppError';
 
+import { AppError } from './shared/errors/AppError';
+import createConnection from "./database/"
+
+import { router } from './routes';
+
+createConnection();
 const app = express();
 
 app.use(cors());
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      // caso o NODE_ENV seja igual a "test" host recebe "localhost", caso contrário host recebe host
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
-      // caso o NODE_ENV seja igual a "test", database receberá rentals_test, se não, utilize o padrão
       database:
         process.env.NODE_ENV === "test"
           ? "fin_api_test"
