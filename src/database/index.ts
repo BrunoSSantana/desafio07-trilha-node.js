1115d56792ab895cbdd4065175ded742fea586c6
BrunoSSantana <brunoosouza15@gmail.com>
2021-07-16 06:37:31 -0300

    Export connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,17 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-(async () => await createConnection())();
+export default async (host = "database_ignite"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      // caso o NODE_ENV seja igual a "test" host recebe "localhost", caso contrário host recebe host
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      // caso o NODE_ENV seja igual a "test", database receberá rentals_test, se não, utilize o padrão
+      database:
+        process.env.NODE_ENV === "test"
+          ? "fin_api_test"
+          : defaultOptions.database,
+    })
+  );
+};
